32dd03fe97a471aca184b52705e9b6638c6d4c98
japa <85523172+J4P4KK@users.noreply.github.com>
2024-08-05 02:01:11 -0300

    e isso ai

---
+++ script/script.js
@@ -0,0 +1,23 @@
+
+
+(function update() {
+    $.getJSON( "https://api.weather.com/v2/pws/observations/current?stationId=IJANDA6&format=json&units=m&apiKey=10714cc84e4040efb14cc84e40b0ef68&numericPrecision=decimal")
+    .done(function( json ) {
+        //console.log( "JSON Data: " + json );
+        $('#windgust').text(json.observations[0].metric.windGust);
+        $('#windspeed').text(json.observations[0].metric.windSpeed);
+        $('#temp').text(json.observations[0].metric.temp);
+        $('#pressure').text(json.observations[0].metric.pressure);
+        $('#heatindex').text(json.observations[0].metric.heatIndex);
+        $('#heatindex').text(json.observations[0].metric.dewpt);
+        $('#humidity').text(json.observations[0].humidity);
+        $('#preciprate').text(json.observations[0].metric.precipRate);
+        $('#preciptotal').text(json.observations[0].metric.precipTotal);
+    })
+    .fail(function( jqxhr, textStatus, error ) {
+        var err = textStatus + ", " + error;
+        console.log( "Request Failed: " + err );
+    }).then(function() {
+        setTimeout(update, 5000);
+    });
+})(); 
